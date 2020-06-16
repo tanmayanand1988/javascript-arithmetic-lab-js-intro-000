5c0b2dfd663c35c246c3e8450e24a3329f3eab68
Tanmay Anand <tanmayanand@gmail.com>
2020-06-16 19:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function multiply(a,b){
 function divide(a,b){
   return a/b
 }
-function increments(n){
+function increment(n){
   return n+=1
 }
-function decrements(n){
+function decrement(n){
   return n-=1
 }
