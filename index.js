4e95ccf2478abc707b3aacbaf6f385ee1c57fa47
Tanmay Anand <tanmayanand@gmail.com>
2020-06-16 19:09:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 function add(a,b){
     return a+b
 }
+function subtract (a,b){
+    return a-b
+}
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+function increments(n){
+  return n+=1
+}
+function decrements(n){
+  return n-=1
+}
